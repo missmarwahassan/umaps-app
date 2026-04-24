bbc97fac6f0501439d7a09e205ca0557d142cd51
Marwa Hassan <marwahas@umich.edu>
2026-04-23 23:47:16 -0400

    updating survey link configuration

--- docs/data/publication-submission-config.js
+++ docs/data/publication-submission-config.js
@@ -1,3 +1,3 @@
 window.UMAPS_PUBLICATION_SUBMISSION = {
-  endpoint: "",
+  endpoint: "https://script.google.com/macros/s/AKfycbwTE7X1yLD9RH3lBuNvvhnyuYnkYPgjyO4RgwelLpWHmROBLraPto-QeMO5z3pUWhD4/exec",
 };
