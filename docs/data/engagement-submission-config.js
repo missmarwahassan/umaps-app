9159ec4f309bc145ef940a47c04d7f6bf09783a9
Marwa Hassan <marwahas@umich.edu>
2026-04-24 00:47:28 -0400

    Fixed config url

--- docs/data/engagement-submission-config.js
+++ docs/data/engagement-submission-config.js
@@ -1,3 +1,3 @@
 window.UMAPS_ENGAGEMENT_SUBMISSION = {
-  endpoint: "",
+  endpoint: "https://script.google.com/macros/s/AKfycbwkLLuDOtAkmTbaO69vqFFJp1-FWOmDw347xaj_Fx3wtVqeZhcV4QZxvMcs-mV4VFI/exec",
 };
